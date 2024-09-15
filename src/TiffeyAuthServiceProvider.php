6caf55605dafb5251ac3144e9b100440a2e40fec
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-09-15 17:26:39 +0100

    Initial commit

---
+++ src/TiffeyAuthServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Permittedleader\FlashMessages;
+
+use Illuminate\Support\ServiceProvider;
+
+class FlashMessagesServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
